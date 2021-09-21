d98501e9502418ec2af43094d81fc8f437a383af
sabrinalvespaiva <sabrina.paiva14@hotmail.com>
2021-09-21 18:04:33 -0300

    primeira landing page com bootstrap

---
+++ assets/JS/script.js
@@ -0,0 +1,78 @@
+/* Case Sensitive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getEmentsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector() usa #
+
+*/
+
+let nome = window.document.getElementById('nome');
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+assunto.style.width = '100%'
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length <= 2){
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+//FUNÇÃO
+function validaEmail(){
+    //DOM
+    let txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 100){
+        txtAssunto.innerHTML = 'Texto muito grande, digite até 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ('Formulario enviado com sucesso!')
+    } else {
+        alert ('Preencha todos os campos do formulário corretamente')
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
